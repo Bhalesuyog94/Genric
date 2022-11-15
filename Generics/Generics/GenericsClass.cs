8c34c2250f955e6a72a2f6e21d9d7b7367aac391
Bhalesuyog94 <113917613+Bhalesuyog94@users.noreply.github.com>
2022-11-15 19:25:27 +0530

    Add files via upload

---
+++ Generics/Generics/GenericsClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class GenericsClass
+    {
+        public class GenericMaximum<T> where T : IComparable
+        {
+            public T[] value;
+
+            public GenericMaximum(T[] value)
+            {
+                this.value = value;
+            }
+            public T[] sort(T[] values)
+            {
+                Array.Sort(values);
+                return values;
+            }
+            public T MaxValue(params T[] values)
+            {
+                var sorted_values = sort(values);
+                return sorted_values[^1];
+            }
+            public void PrintMaxvalue()
+            {
+                var max = MaxValue(this.value);
+                Console.WriteLine("Maximum vaule is " +max);
+            }
+        }
+    }
+}
---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Generics;
+using static Generics.GenericsClass;
+//int[] arr = { 112, 344, 432, 555, 678 };
+//GenericMaximum<int> generic = new GenericMaximum<int>(arr);
+//generic.PrintMaxvalue();
+Console.WriteLine("Find MAX Number");
+int output = MaximumInt.MaximumIntegerNumber(11, 22, 33);
+Console.WriteLine(output);
+double doubleoutput = MaxiumFloat.MaximumfloatNumber(11.22, 22.33, 33.44);
+Console.WriteLine(doubleoutput);
+string stringoutput = MaximumStrings.MaximumString("22", "33", "44");
+Console.WriteLine(stringoutput);
+
+
